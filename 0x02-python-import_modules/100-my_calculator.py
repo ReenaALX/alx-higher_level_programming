c5874752a9a6502994f60f22239cad56631f4174
ReenaALX <m9991333@gmail.com>
2023-11-30 13:26:38 -0800

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        sys.exit(1)
+    op = sys.argv[2]
+    if op not in '+-*/':
+        error = 'Unknown operator. Available operators: +, -, * and /'
+        print(error)
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    result = 0
+    if op == '+':
+        result = add(a, b)
+    elif op == '-':
+        result = sub(a, b)
+    elif op == '*':
+        result = mul(a, b)
+    else:
+        result = div(a, b)
+    print("{} {} {} = {}".format(a, op, b, result))
